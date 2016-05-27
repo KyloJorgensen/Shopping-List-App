5179c0b8d7695ae263ed32e0f67b4cf437531496
KyloJorgensen <kylo.j.jorgensen@gmail.com>
2016-05-26 22:59:05 -0700

    add body back ground

--- js/app.js
+++ js/app.js
@@ -1,26 +1,42 @@
 $(document).ready(function() {
 
 	$('ul').on('click', '.icon-check-empty', function(){
-		$(this).parent().children('p').css("text-decoration", "line-through")
+		console.log($(this));
+		$(this).parent().children('input').addClass('crossout')
 		.parent().children('.icon-check-empty').hide()
 	    .parent().children('.icon-check').show();
 	});
 
 	$('ul').on('click', '.icon-check', function(){
-		$(this).parent().children('p').css("text-decoration", "none")
+		$(this).parent().children('input').removeClass('crossout')
 		.parent().children('.icon-check-empty').show()
 		.parent().children('.icon-check').hide();
 	});
 
-	$('ul').on('click', '.icon-trash', function(){
-		$(this).parent().hide();
+	$('ul').on('click', '#list-item', function(){
+		$(this).parent().children('.icon-trash').show()
+		.parent().children('.icon-plus').show()
+		.parent().children('.icon-check').hide()
+		.parent().children('.icon-check-empty').hide();
 	});
 
-	$('ul').on('click', '#list-item', function(){
-		$(this).parent().children('.icon-trash').show();
-		$(this).parent().children('.icon-plus').show();
-		$(this).parent().children('.icon-check').hide();
-		$(this).parent().children('.icon-check-empty').hide();
+	$('.add').click(function(){
+		$('ul').append('<li><i class="replace icon-plus"></i><i class="icon-check"></i><i class="icon-check-empty"></i><i class="icon-trash"></i><input type="text" id="list-item" placeholder="' + $('#user-input').val() + '"></input></li>');
+		$(this).parent().children('input').hide()
+		.parent().children('i').after('<input type="text" id="user-input" placeholder="Click here to add new item."></input>');
+	});	
+
+	$('ul').on('click', '.replace', function(){
+		$(this).parent().children('input').hide()
+		.parent().children('.icon-trash').after('<input type="text" id="list-item" placeholder="' + $(this).parent().children('#list-item').val() + '"></input>')
+		.parent().children('.icon-trash').hide()
+		.parent().children('.icon-plus').hide()
+		.parent().children('.icon-check').hide()
+		.parent().children('.icon-check-empty').show();
+	});
+
+	$('ul').on('click', '.icon-trash', function(){
+		$(this).parent().hide();
 	});
 
 	$('.help').click(function(){
@@ -33,14 +49,6 @@ $(document).ready(function() {
 		$('.help-box').hide();
 	});
 
-	$('.add').click(function(){
-		$('ul').append('<li><i class="icon-check"></i><i class="icon-check-empty"></i><i class="icon-trash"></i><p>' + $('#user-input').val() + '</p></li>')
-		
-	});
-
-	$('ul').on('click', '.replace', function(){
-		alert($('this'));
-	});
 });
 
 
